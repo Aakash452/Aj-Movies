98eeb411ff550da7061fff0451ef9f353478d7b4
Aakash <aakashseth454@gmail.com>
2021-06-05 16:01:51 +0530

    Updates

---
+++ precache-manifest.c83cf25bdd26aa0f54e8a954ad4fe7a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdec2ef1dc501002cf43c96904fe8fba",
+    "url": "/Aj-Movies/index.html"
+  },
+  {
+    "revision": "ab9cc4791dd3b24865c9",
+    "url": "/Aj-Movies/static/css/main.d28fc6b7.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/Aj-Movies/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Aj-Movies/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9cc4791dd3b24865c9",
+    "url": "/Aj-Movies/static/js/main.6897783d.chunk.js"
+  },
+  {
+    "revision": "0669ce9fa2890a9ec1df",
+    "url": "/Aj-Movies/static/js/runtime-main.1d1a7688.js"
+  }
+]);
